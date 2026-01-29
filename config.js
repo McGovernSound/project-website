6321c581117df2662cfd7abbcd2dc1eae3303e74
Devin McGovern <devinmcgovern@Devins-MacBook-Pro.local>
2026-01-28 21:50:59 -0500

    Update repository names to match GitHub

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     githubUsername: "McGovernsound",
     repositories: [
-        "Expense-tracker",
-        "Inventory-system-desktop",
-        "amp-monitor-desktop",
-        "line-draw-desktop",
-        "rack-designer-desktop"
+        "trkr-expense-dist",
+        "trkr-inventory-dist",
+        "amp-monitor-dist",
+        "Rack-Designer",
+        "line-draw-desktop"
     ],
     siteTitle: "Devin's Projects",
     siteSubtitle: "Download the latest versions of my software projects."
