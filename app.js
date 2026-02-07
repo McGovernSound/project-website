28886b22623992de9ca357a3b2e5c56944947901
Devin McGovern <devinmcgovern@Devins-MacBook-Pro.local>
2026-02-06 19:37:09 -0500

    Hard bust config.js cache

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import CONFIG from './config.js';
+import CONFIG from './config.js?v=1.1.0';
 
 async function fetchProjectData(projectObj) {
     const { repo: repoName, displayName } = projectObj;
