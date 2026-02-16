34da7fb38d217ee546aaf5f507ddebae944fe06f
Devin McGovern <devinmcgovern@Devins-MacBook-Pro.local>
2026-02-15 19:50:42 -0500

    Rename project display names to MGS prefix

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     githubUsername: "McGovernsound",
     repositories: [
-        { repo: "trkr-expense-dist", displayName: "MSG Expense Tracker" },
-        { repo: "trkr-inventory-dist", displayName: "MSG Inventory System" },
-        { repo: "amp-monitor-dist", displayName: "MSG Amp Monitor" },
-        { repo: "Rack-Designer", displayName: "MSG Rack Designer" },
-        { repo: "line-draw-desktop-dist", displayName: "MSG Signal Flow" }
+        { repo: "trkr-expense-dist", displayName: "MGS Expense Tracker" },
+        { repo: "trkr-inventory-dist", displayName: "MGS Inventory System" },
+        { repo: "amp-monitor-dist", displayName: "MGS Amp Monitor" },
+        { repo: "Rack-Designer", displayName: "MGS Rack Designer" },
+        { repo: "line-draw-desktop-dist", displayName: "MGS Signal Flow" }
     ],
     siteTitle: "MGS's Latest",
     siteSubtitle: "Download the latest versions of the MGS suite."
